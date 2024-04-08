0537cd58923b89af0e49fda27b2b24dceb812eea
Rajath Rao <raorajath1998@gmail.com>
2024-04-09 02:26:54 +0530

    added suspense and vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { lazy } from "react";
+import { Suspense, lazy } from "react";
 import { Route, Switch } from "react-router-dom";
 // import RecipePage from "./components/RecipePage/RecipePage";
 import Main from "./components/Main";
@@ -10,12 +10,14 @@ function App() {
   return (
     <>
       <Switch>
+        <Suspense fallback={<p>loading... Please wait</p>}>
         <Route path="/" exact>
           <Main />
         </Route>
         <Route path="/recipe-page">
           <RecipePage />
         </Route>
+        </Suspense>
       </Switch>
     </>
   );
